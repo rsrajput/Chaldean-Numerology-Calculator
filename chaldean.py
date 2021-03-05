1bb50b746894d19135d58c5bb6991ed0efc2df72
rsrajput <rsrajput@gmail.com>
2021-03-05 18:22:10 +0530

    Update chaldean.py in 2021

--- chaldean.py
+++ chaldean.py
@@ -1,16 +1,35 @@
-# Prints name value based on 1. Alphabets positional value 2. Chaldean numerology name number
+# Prints your Chaldean numerology name number & also Cheiro/Linda Goodman name number.
+from collections import defaultdict
 
-alphabets = {' ':0, "a":1, "b": 2,"c": 3,"d": 4,"e": 5,"f": 6,"g": 7,"h": 8,"i": 9,"j": 10,"k": 11,"l": 12,"m": 13,"n": 14,"o": 15,"p": 16,"q": 17,"r": 18,"s": 19,"t": 20,"u": 21,"v": 22,"w": 23,"x": 24,"y": 25,"z": 26}
 chaldean = {' ':0, "a":1, "b": 2,"c": 3,"d": 4,"e": 5,"f": 8,"g": 3,"h": 5,"i": 1,"j": 1,"k": 2,"l": 3,"m": 4,"n": 5,"o": 7,"p": 8,"q": 1,"r": 2,"s": 3,"t": 4,"u": 6,"v": 6,"w": 6,"x": 5,"y": 1,"z": 7}
+chaldean = defaultdict(lambda: 0, chaldean)
 
-name = list(input("Please enter your name:  "))
-sum = 0
-for x in range(len(name)):
-	sum = sum + alphabets[name[x]]
+name_input = input("Please enter your name:  ")
+name_input = name_input.lower()
+split_name = []
+name_split = name_input.split()
+for i in name_split:
+	split_name.append(i)
 
-sum_c = 0
-for y in range(len(name)):
-	sum_c = sum_c + chaldean[name[y]]
+indiv = []
+indiv_chiero =[]
 
-print('Positional Sum: ', sum)
-print('Chaldean Numerology: ', sum_c)
+for j in split_name:
+	name = list(j)
+	sum_c = 0
+	for y in range(len(name)):
+		sum_c = sum_c + chaldean[name[y]]
+
+	indiv.append(sum_c)
+
+	if (sum_c == 11) or (sum_c == 22):
+		indiv_chiero.append(sum_c)
+	else: 
+		indiv_chiero.append(sum(map(int, str(sum_c))))
+
+
+total  = sum(indiv)
+total_chiero  = sum(indiv_chiero)
+
+print('Chaldean Numerology: {} & {} for {}'.format(indiv, total, name_input.title()))
+print('ChieroLG Numerology: {} & {} for {}'.format(indiv_chiero, total_chiero, name_input.title()))
